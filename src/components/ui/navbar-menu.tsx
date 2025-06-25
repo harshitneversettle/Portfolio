bb66691229c6721f567457804d8e551d4e796860
Harshit Yadav <harshityadav5499@gmail.com>
2025-06-25 12:23:15 +0530

    removed bugs

--- src/components/ui/navbar-menu.tsx
+++ src/components/ui/navbar-menu.tsx
@@ -1,10 +1,11 @@
 "use client";
 import React from "react";
 import { motion } from "motion/react";
+import { Transition } from "framer-motion";
 
 
 
-const transition = {
+const transition : Transition = {
   type: "spring",
   mass: 0.5,
   damping: 11.5,
